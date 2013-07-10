101898a030a1010d96c6be45c9c1a00ed2700aaf
Daniel Kraft <d@domob.eu>
2013-07-10 21:49:56 +0200

    Add compile script and ChangeLog.
    Add a compilation script to generate the XPI, and write
    a ChangeLog entry for 0.1a to be released now.

---
+++ firefox/compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#   NameID, a namecoin based OpenID identity provider.
+#   Copyright (C) 2013 by Daniel Kraft <d@domob.eu>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Compile the add-on into an XPI.
+
+dir=login@nameid.org
+zipfile=NameIdLogin-0.1a.xpi
+
+files="bootstrap.js chrome.manifest install.rdf"
+for file in Namecoind.js NameIdAddon.js \
+            preferences.js preferences.xul \
+            PrefHandler.js Utils.js
+do
+  files+=" content/$file"
+done
+
+(cd $dir; zip ../$zipfile $files)
