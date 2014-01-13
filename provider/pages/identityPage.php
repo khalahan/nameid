6f62d47c2e06b6c00ae01df7ba4d6d2b50f73285
Daniel Kraft <d@domob.eu>
2014-01-13 21:30:25 +0100

    Handle GPG in identity page.

--- provider/pages/identityPage.php
+++ provider/pages/identityPage.php
@@ -22,6 +22,38 @@
 if (!isset ($fromIndex) || $fromIndex !== "yes")
   die ("Invalid page load.\n");
 
+/* Sanitise a link.  This makes sure that it starts with 'http://'
+   or 'https://', in order to prevent in particular javascript: links
+   to be inserted by an attacker.  */
+function sanitiseLink ($href)
+{
+  $allowed = array ("http://", "https://");
+  foreach ($allowed as $prefix)
+    if (substr ($href, 0, strlen ($prefix)) === $prefix)
+      return $href;
+
+  return "http://$href";
+}
+
+/* Helper routine to check a GPG fingerprint and format it nicely.  This
+   makes all hex characters upper case, removes spaces and colons, and
+   puts back spaces to group the characters in a uniform way.  Finally,
+   it formats the final digits (as in the usual GPG fingerprint key ID)
+   in <strong> tags.  */
+function formatGPG ($fpr)
+{
+  $fpr = strtoupper ($fpr);
+  $fpr = preg_replace ("/[ :]/", "", $fpr);
+
+  if (!preg_match ("/^[0-9A-F]{40}$/", $fpr))
+    return NULL;
+
+  $fpr = preg_replace ("/(.{4})/", "$1 ", $fpr);
+  $fpr = preg_replace ("/(.{4} .{4}) $/", "<strong>$1</strong>", $fpr);
+
+  return $fpr;
+}
+
 ?>
 
 <h1><?php echo $html->escape ("$namePrefix/$identityName"); ?></h1>
@@ -32,6 +64,7 @@ $displayInfo = array ("name" => "Real Name",
                       "nick" => "Nickname",
                       "website" => "Website",
                       "email" => "Email",
+                      "gpg" => "OpenPGP",
                       "bitmessage" => "Bitmessage",
                       "xmpp" => "XMPP",
                       "bitcoin" => "Bitcoin",
@@ -57,12 +90,39 @@ public profile information registered:</p>
               {
               case "website":
                 $text = $html->escape ($identityPage->$key);
-                $content = "<a href='$text'>$text</a>";
+                $href = $html->escape (sanitiseLink ($identityPage->$key));
+                $content = "<a href='$href'>$text</a>";
                 break;
+
               case "email":
                 $text = $html->escape ($identityPage->$key);
                 $content = "<a href='mailto:$text'>$text</a>";
                 break;
+
+              case "gpg":
+                if (is_object ($identityPage->$key)
+                    && isset ($identityPage->$key->v)
+                    && $identityPage->$key->v === "pka1"
+                    && isset ($identityPage->$key->fpr))
+                  {
+                    $val = formatGPG ($identityPage->$key->fpr);
+                    if ($val !== NULL)
+                      {
+                        $href = NULL;
+                        $content = "";
+                        if (isset ($identityPage->$key->uri))
+                          {
+                            $href = $identityPage->$key->uri;
+                            $href = $html->escape (sanitiseLink ($href));
+                            $content .= "<a href='$href'>";
+                          }
+                        $content .= $val;
+                        if ($href !== NULL)
+                          $content .= "</a>";
+                      }
+                  }
+                break;
+
               default:
                 $content = $html->escape ($identityPage->$key);
                 break;
